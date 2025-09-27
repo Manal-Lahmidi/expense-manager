53f36abe644467d0cd893c4e3abcf34b0605dea8
Manal-Lahmidi <manallahmidi40@gmail.com>
2025-09-28 00:03:54 +0100

    fixed contextLoads()

--- src/test/java/com/manal/expensemanager/ExpenseManagerApplicationTests.java
+++ src/test/java/com/manal/expensemanager/ExpenseManagerApplicationTests.java
@@ -1,16 +1,16 @@
 package com.manal.expensemanager;
 
+import com.manal.expensemanager.testsupport.PostgresITBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+class ExpenseManagerApplicationTests extends PostgresITBase {
 
-class ExpenseManagerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // boots the Spring context with the Testcontainers Postgres from PostgresITBase
+    }
 }

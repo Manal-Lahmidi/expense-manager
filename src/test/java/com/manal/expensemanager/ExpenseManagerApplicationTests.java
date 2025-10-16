b71c8fc121a3a4efdfa3bb2416918285d8d4d70e
Manal-Lahmidi <manallahmidi40@gmail.com>
2025-10-16 20:11:50 +0100

    ci fixed

--- src/test/java/com/manal/expensemanager/ExpenseManagerApplicationTests.java
+++ src/test/java/com/manal/expensemanager/ExpenseManagerApplicationTests.java
@@ -3,10 +3,21 @@ package com.manal.expensemanager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        // don’t auto-configure a DataSource for this smoke test
-        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
-})
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = {
+                // Turn off anything that would try to touch a DB
+                "spring.autoconfigure.exclude=" +
+                        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration," +
+                        "org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration",
+                // Stop Spring Boot 3.3 testcontainers auto-service-connection magic
+                "spring.testcontainers.enabled=false"
+        }
+)
+// DO NOT extend PostgresITBase here
+// DO NOT set @ActiveProfiles here
 class ExpenseManagerApplicationTests {
 
     @Test

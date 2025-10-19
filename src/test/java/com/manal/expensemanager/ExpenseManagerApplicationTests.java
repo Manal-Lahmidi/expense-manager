47ace51f77ceab89d8fa2591879499494c5da8d9
Manal-Lahmidi <manallahmidi40@gmail.com>
2025-10-19 15:15:01 +0100

    something

--- src/test/java/com/manal/expensemanager/ExpenseManagerApplicationTests.java
+++ src/test/java/com/manal/expensemanager/ExpenseManagerApplicationTests.java
@@ -1,21 +1,13 @@
 package com.manal.expensemanager;
 
+import com.manal.expensemanager.testsupport.PostgresITBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.NONE,
-        properties = {
-                "spring.testcontainers.enabled=false",
-                "spring.autoconfigure.exclude=" +
-                        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration"
-        }
-)
-class ExpenseManagerApplicationTests {
-    @org.springframework.boot.test.mock.mockito.MockBean
-    com.manal.expensemanager.repository.UserRepository userRepository;
-    @org.junit.jupiter.api.Test void contextLoads() {}
+@SpringBootTest
+@ActiveProfiles("test")
+class ExpenseManagerApplicationTests extends PostgresITBase {
+    @Test void contextLoads() {}
 }
+
